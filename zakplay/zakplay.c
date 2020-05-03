3e33111d7e0287bbf39773403c1db21ecab5b802
Jari Komppa <jari.komppa@gmail.com>
2020-05-03 11:20:17 +0300

    Too many changes for this comment message to contain.

--- zakplay/zakplay.c
+++ zakplay/zakplay.c
@@ -9,9 +9,14 @@
  /*
  State machine:
  0 - ISR playing from buffer A, app should just wait. ISR 0->1
+ 0 - ISR notices the song ends                        ISR 0->4
+ 0 - ISR notices the song ends and wants to loop      ISR 0->5
  1 - ISR moved to buffer B, app should copy B to A.   App 1->2
  2 - App copied B to A, ISR should move to A.         ISR 2->3
  3 - ISR moved to A, app should fill buffer B.        App 3->0
+ 4 - Shutdown and quit                                n/a
+ 5 - App copies loop point to buffers a and b         App 5->6
+ 6 - ISR moved to A/B                                 ISR 6->0 or 6->1 
  */
 
 
@@ -22,6 +27,10 @@
 #include "yofstab.h"
 const unsigned char propfont[] = {
 #include "font_elegante_pixel.h"
+};
+
+const unsigned char zaklogo[] = {
+#include "zaklogo.h"
 };
 
 extern void drawstringz(unsigned char *aS, unsigned char aX, unsigned char aY);
@@ -61,7 +70,7 @@ void printnum(unsigned char v, unsigned char x, unsigned char y)
 }
 
 /*
-rather wasteful memory layout
+our rather wasteful memory layout
 0xe000 - nextreg original values
 0xe100 - allocated page handles
 0xe200 - state variables
@@ -79,12 +88,20 @@ __at (0xe201) unsigned short ofs;
 __at (0xe203) unsigned char framedelay;
 __at (0xe204) unsigned char state;
 __at (0xe205) unsigned short srcofs;
+__at (0xe207) unsigned short kblock;
 __at (0xe250) unsigned char debugvalue;
 
 __at (0xe300) unsigned char ayregs[48];
 __at (0xe400) unsigned char buffer_a[1024];
 __at (0xe800) unsigned char buffer_b[1024];
 __at (0xf000) unsigned char zakheader[100];
+__at (0xf000+10) unsigned char zak_chiptype;
+__at (0xf000+11) unsigned char zak_flags;
+__at (0xf000+12) unsigned short zak_kblocks;
+__at (0xf000+14) unsigned short zak_lastblock;
+__at (0xf000+16) unsigned short zak_loopblock;
+__at (0xf000+18) unsigned short zak_loopbyte;
+
 
 
 char memcmp(char *a, char *b, unsigned short l)
@@ -149,6 +166,12 @@ void copybufbtoa()
 
 void fillbufb()
 {
+    if (kblock == zak_kblocks)
+    {
+        memset(buffer_b, 0, 1024);
+        return;
+    }
+    kblock++;
     writenextreg(0x55, pages[2 + activepage]);
     writenextreg(0x56, pages[2 + activepage + 1]);
     //memcpy(buffer_b, (unsigned char*)srcofs, 1024);    
@@ -160,6 +183,19 @@ void fillbufb()
     {
         srcofs -= 8192;
         activepage++;
+    }
+}
+
+void zeroay()
+{
+    unsigned char r, c;
+    for (c = 0; c < 3; c++)
+    {
+        setaychip(c); // AY 0, 1 or 2
+        for (r = 0; r < 14; r++)
+        {
+            aywrite(r, 0);
+        }
     }
 }
 
@@ -182,9 +218,20 @@ void isr()
     {
         val = buffer_a[ofs]; ofs++;
         reg = buffer_a[ofs]; ofs++;
-        if (ofs == 1024)
+        if (kblock == zak_kblocks)
         {
-            state = 1;
+            if (ofs >= 1024 + zak_lastblock)
+            {
+                // todo: handle looping
+                state = 4;
+            }
+        }
+        else
+        {
+            if (ofs == 1024)
+            {
+                state = 1;
+            }
         }
         if (reg < 48)
         {
@@ -193,7 +240,8 @@ void isr()
             aywrite(reg & 15, val); // low 4 bits is reg number
             //port254(val & 7);
         }
-    } while ((reg & 0x80) == 0);
+
+    } while ((reg & 0x80) == 0);    
     framedelay = val - 1;   
 }
 
@@ -231,10 +279,10 @@ char checkhdr(char f)
         return 1;
     }
     // is this an AY/YM file?
-    if (!(zakheader[10] == 1 ||
-          zakheader[10] == 2 ||
-          zakheader[10] == 3) ||
-          (zakheader[11] & 2) == 2)
+    if (!(zak_chiptype == 1 ||
+          zak_chiptype == 2 ||
+          zak_chiptype == 3) ||
+          (zak_flags & 2) == 2)
     {
         return 2;
     }
@@ -249,9 +297,22 @@ char checkhdr(char f)
     }
     
     // Select AY or FM based on song flag
-    writenextreg(0x06, (readnextreg(0x06) & 0xfc) + (zakheader[11] & 64) ? 0 : 1);
+    writenextreg(0x06, (readnextreg(0x06) & 0xfc) + (zak_flags & 64) ? 0 : 1);
+    
+    if (zak_flags & 64) 
+    {
+        drawstringz("AY-3-8910", 0, 20);
+    }
+    else
+    {
+        drawstringz("YM2149F", 0, 20);
+    }
+    if (zak_chiptype == 1) drawstringz("Normal", 0, 21);
+    if (zak_chiptype == 2) drawstringz("Turbosound", 0, 21);
+    if (zak_chiptype == 3) drawstringz("Turbosound Next", 0, 21);      
+    
     // read strings into buffer, sanitize strings and print strings on screen
-    ofs = readstring(f, 4);
+    ofs = readstring(f, 5);
     ofs = readstring(f, ofs);
     readstring(f, ofs);
     // at this point we're ready to load the data
@@ -276,13 +337,30 @@ void vis()
 {
     unsigned char i;
     unsigned char j;
-    unsigned char prog = ofs >> 5;
+    unsigned char prog;
+    unsigned char mask;
+    unsigned short goal;
+    unsigned short acc;
     
+    // no div, no problem.... kinda
+    acc = 0;
+    goal = kblock * 32 * 8;
+    prog = 0;
+    while (goal > acc)
+    {
+        acc += zak_kblocks;
+        prog++;
+    }
+    
+    mask = prog & 7;
+    mask = 0xff << (7-mask);
+    prog >>= 3;
+
     for (i = 0; i < 32; i++)
     {
-        *((unsigned char *)yofs[23] + i) = (i >= prog) ? 0 : 0xff;
+        *((unsigned char *)yofs[23] + i) = (i > prog) ? 0 : ((i < prog) ? 0xff : mask);
     }
-
+        
     prog = (srcofs >> 8) & 31;
     
     for (i = 0; i < 32; i++)
@@ -290,22 +368,66 @@ void vis()
         *((unsigned char *)yofs[23] + i + 512) = (i >= prog) ? 0 : 0xff;
     }
 
-    for (i = 0; i < 4; i++)
+    prog = ofs >> 5;
+    
+    for (i = 0; i < 32; i++)
     {
-        *((unsigned char *)yofs[23] + i + 1024) = (i == state) ? 0xff : 0;
+        *((unsigned char *)yofs[23] + i + 1024) = (i >= prog) ? 0 : 0xff;
     }
     
+    // register dots
     for (j = 0; j < 3; j++)
     {
-        for (i = 0; i < 16; i++)
-            *((unsigned char *)yofs[18 + j] + i) = ayregs[j*16+i];    
+        for (i = 0; i < 4; i++)
+        {
+            *((unsigned char *)yofs[5 + j*4] + 28 + i) = ayregs[j*16+i] & 0x55;
+            *((unsigned char *)yofs[5 + j*4] + 28 + i + 512) = (ayregs[j*16+i] >> 1) & 0x55;
+            *((unsigned char *)yofs[5 + j*4+1] + 28 + i) = ayregs[j*16+i+4] & 0x55;
+            *((unsigned char *)yofs[5 + j*4+1] + 28 + i + 512) = (ayregs[j*16+i+4] >> 1) & 0x55;
+            *((unsigned char *)yofs[5 + j*4+2] + 28 + i) = ayregs[j*16+i+8] & 0x55;
+            *((unsigned char *)yofs[5 + j*4+2] + 28 + i + 512) = (ayregs[j*16+i+8] >> 1) & 0x55;
+            *((unsigned char *)yofs[5 + j*4+3] + 28 + i) = ayregs[j*16+i+12] & 0x55;
+            *((unsigned char *)yofs[5 + j*4+3] + 28 + i + 512) = (ayregs[j*16+i+12] >> 1) & 0x55;
+
+        }
+    }
+}
+
+void openfile(char *f)
+{
+    unsigned char temp[128];
+    unsigned char i;
+    i = 0;
+    while (i < 127 && cmdline[i] != 0 && cmdline[i] != 0xd && cmdline[i] != ':')
+    {
+        temp[i] = cmdline[i];
+        i++;
+    }
+    temp[i] = 0;
+    drawstringz(temp, 0, 3);    
+    *f = fopen(temp, 1);
+}
+
+void drawlogo()
+{
+    unsigned char x,y,c;
+    for (c = 0, y = 0; y < 58; y++)
+    {
+        for (x = 0; x < 4; x++, c++)
+        {
+            *((unsigned char*)(yofs[0]+y*32+x+28)) = zaklogo[c];
+        }
     }
 }
 
 void main()
 {     
     char f;
-    char r;    
+    char r;
+    memset((unsigned char*)yofs[0],0,192*32);
+    memset((unsigned char*)yofs[0]+192*32,7,24*32);
+    gPort254 = 0;
+    drawlogo();
     r = allocpage();
     f = readnextreg(0x57);
     writenextreg(0x57, r);    
@@ -317,7 +439,7 @@ void main()
     pages[0] = 1; // number of allocated pages
     pages[1] = r; // allocated top page
     writenextreg(0x07, 3); // set speed to 28MHz
-    f = fopen("adversary.zak", 1);
+    openfile(&f);
     r = checkhdr(f);
     if (r)
     {
@@ -344,6 +466,7 @@ void main()
         srcofs = 0xa000;
         framedelay = 0;
         state = 0;
+        kblock = 0;
 
         fillbufb();
         copybufbtoa();
@@ -352,7 +475,7 @@ void main()
         setupisr7();
         readkeyboard();
         ei();
-        while (!KEYDOWN(SPACE))
+        while (!KEYDOWN(SPACE) && state != 4)
         {
             readkeyboard();
             vis(); 
@@ -370,6 +493,7 @@ void main()
         di();    
         closeisr7();
     }
+    zeroay(); // silence, please
     writenextreg(0x06, nextregbackup[0x06]);
     writenextreg(0x07, nextregbackup[0x07]);
     for (r = 0; r < pages[0]; r++)
