deb419301a40f20cbd066101a42dcffd3afc4060
Jari Komppa <jari.komppa@gmail.com>
2020-05-18 15:40:55 +0300

    timestamps in log, syncpoint file

--- sync/nextsync.py
+++ sync/nextsync.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import datetime
 import fnmatch
 import socket
 import glob
@@ -8,9 +9,24 @@ import os
 PORT = 2048    # Port to listen on (non-privileged ports are > 1023)
 VERSION = "NextSync1"
 IGNOREFILE = "syncignore.txt"
+SYNCPOINT = "syncpoint.dat"
 
+def touch(fname):
+    if os.path.exists(fname):
+        os.utime(fname, None)
+    else:
+        open(fname, 'a').close()
 
-def getFileList():
+def agecheck(f):
+    if not os.path.isfile(SYNCPOINT):
+        return False
+    ptime = os.path.getmtime(SYNCPOINT)
+    ftime = os.path.getmtime(f)
+    if ftime > ptime:
+        return False
+    return True
+
+def getFileList():    
     ignorelist = []
     if os.path.isfile(IGNOREFILE):
         with open(IGNOREFILE) as f:
@@ -23,10 +39,15 @@ def getFileList():
             for i in ignorelist:
                 if fnmatch.fnmatch(g, i):
                     ignored = True
+            if agecheck(g):
+                ignored = True
             if not ignored:
                 stats = os.stat(g)
                 r.append([g, stats.st_size])
     return r;
+
+def timestamp():
+    return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 print("NextSync server, protocol version "+VERSION)
 print("by Jari Komppa 2020")
@@ -45,41 +66,46 @@ if hostinfo[2] != []:
 print()
 if not os.path.isfile(IGNOREFILE):
     print("Warning! Ignore file "+IGNOREFILE+" not found in directory. All files will be synced.")
-    print()
+if not os.path.isfile(SYNCPOINT):
+    print("Note: Sync point file "+SYNCPOINT+" not found, syncing all files regardless of timestamp.")
+print("Note: Ready to sync", len(getFileList()), "files.")
+print()
 
 while True:
-    print("NextSync listening to port", PORT)
+    print(timestamp(),"| NextSync listening to port", PORT)
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind(("", PORT))
         s.listen()
         conn, addr = s.accept()
         f = getFileList()
+        print(timestamp(), '| Sync file list has', len(f), 'files.')
         fn = 0;
         with conn:
-            print('Connected by', addr)
+            print(timestamp(), '| Connected by', addr[0], 'port', addr[1])
             working = True
             while working:
                 data = conn.recv(1024)
                 if not data:
                     break
                 decoded = data.decode()
-                print('Data received: "' + decoded + '",',len(decoded), 'bytes')
+                print(timestamp(), '| Data received: "' + decoded + '",',len(decoded), 'bytes')
                 if data == b"Sync":
-                    print('Sending "'+VERSION+'"')
+                    print(timestamp(), '| Sending "'+VERSION+'"')
                     conn.sendall(str.encode(VERSION))
                 else:
                     if data == b"Next":
                         if fn >= len(f):
-                            print("Nothing to sync")
+                            print(timestamp(), "| Nothing (more) to sync")
                             conn.sendall(b'\x00\x00\x00\x00\x00') # end of.
+                            touch(SYNCPOINT) # Sync complete, set sync point
                         else:
-                            print("file:", f[fn][0], "len:",f[fn][1])
+                            print(timestamp(), "| File:", f[fn][0], "length:",f[fn][1])
                             packet = (f[fn][1]).to_bytes(4, byteorder="big") + (len(f[fn][0])).to_bytes(1, byteorder="big") + f[fn][0].encode()
                             #print(packet)
                             conn.sendall(packet)
                             fn+=1
                     else:
-                        print("Unknown command")
+                        print(timestamp(), "| Unknown command")
                         conn.sendall(str.encode("Error"))
-    print("Disconnected")
+    print(timestamp(), "| Disconnected")
     print()
